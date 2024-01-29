e09401841dbec06e40db3815c1cf66b9d04eff46
Steele Scott <60533996+steele123@users.noreply.github.com>
2024-01-29 07:21:03 -0500

    add updating embed

--- Bot/EmbedDisplayPlayer.cs
+++ Bot/EmbedDisplayPlayer.cs
@@ -1,4 +1,6 @@
-﻿using DSharpPlus.Entities;
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 using Lavalink4NET.Players;
 using Lavalink4NET.Players.Queued;
 using Lavalink4NET.Tracks;
@@ -23,12 +25,31 @@ public class EmbedDisplayPlayer(IPlayerProperties<EmbedDisplayPlayer, EmbedDispl
             return;
         }
 
-        await EmbedMessage.ModifyAsync(embed: new DiscordEmbedBuilder()
-            .WithTitle(queueItem.Track!.Title)
-            .WithDescription(queueItem.Track.Author)
-            .WithThumbnail(queueItem.Track.ArtworkUri)
-            .WithUrl(queueItem.Track.Uri)
-            .WithBranding());
+        await EmbedMessage.ModifyAsync(CreateMessage(queueItem.Track!, RepeatMode != TrackRepeatMode.None, IsPaused));
+    }
+
+    public override async ValueTask PauseAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        await base.PauseAsync(cancellationToken);
+
+        if (EmbedMessage is null)
+        {
+            return;
+        }
+
+        await EmbedMessage.ModifyAsync(CreateMessage(CurrentTrack!, RepeatMode != TrackRepeatMode.None, IsPaused));
+    }
+
+    public override async ValueTask ResumeAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        await base.ResumeAsync(cancellationToken);
+
+        if (EmbedMessage is null)
+        {
+            return;
+        }
+
+        await EmbedMessage.ModifyAsync(CreateMessage(CurrentTrack!, RepeatMode != TrackRepeatMode.None, IsPaused));
     }
 
     public override async ValueTask StopAsync(CancellationToken cancellationToken = new())
@@ -40,13 +61,50 @@ public class EmbedDisplayPlayer(IPlayerProperties<EmbedDisplayPlayer, EmbedDispl
             return;
         }
 
-        await EmbedMessage.ModifyAsync(embed: new DiscordEmbedBuilder()
-            .WithDescription("That's a wrap, you've reached the end of the queue bro.")
-            .WithBranding());
-        
+        try
+        {
+            var embed = new DiscordEmbedBuilder()
+                .WithDescription("That's a wrap, you've reached the end of the queue bro.\n`/play` to add more songs.")
+                .WithBranding();
+
+            var msg = new DiscordMessageBuilder()
+                .AddEmbed(embed);
+
+            await EmbedMessage.ModifyAsync(msg);
+        }
+        catch (NotFoundException)
+        {
+            // Message was deleted..
+            EmbedMessage = null;
+            return;
+        }
+
+
         // Delete message after 10 seconds
         await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
         await EmbedMessage.DeleteAsync();
+    }
+
+    public static DiscordMessageBuilder CreateMessage(LavalinkTrack track, bool repeat, bool paused)
+    {
+        return new DiscordMessageBuilder()
+            .WithEmbed(new DiscordEmbedBuilder()
+                .WithTitle(track.Title)
+                .WithUrl(track.Uri)
+                .WithThumbnail(track.ArtworkUri)
+                .AddField("Duration", track.Duration.ToString(), true)
+                .AddField("Author", track.Author, true)
+                .AddField("Paused", paused ? "Yes" : "No", true)
+                .AddField("Repeat", repeat ? "Yes" : "No", true)
+                .WithBranding()
+            )
+            .AddComponents(new DiscordComponent[]
+            {
+                new DiscordLinkButtonComponent(track.Uri?.ToString(), "Link"),
+                new DiscordButtonComponent(ButtonStyle.Success, "play/pause", paused ? "Play" : "Pause"),
+                new DiscordButtonComponent(ButtonStyle.Danger, "skip", "Skip"),
+                new DiscordButtonComponent(ButtonStyle.Secondary, "repeat", repeat ? "Repeat Off" : "Repeat On")
+            });
     }
 }
 
